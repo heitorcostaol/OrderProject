c6780fa92fc8473bc26d859803d41521a97eb841
Heitor Costa Oliveira <heitorcostaol@gmail.com>
2020-06-04 20:15:12 -0300

    ProjetoFinal

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,17 +1,18 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+	private static final SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+	
 	private String name;
 	private String email;
 	private Date birthDate;
 
-
 	public Client() {
 		
 	}
-
 
 	public Client(String name, String email, Date birthDate) {
 		this.name = name;
@@ -48,6 +49,12 @@ public class Client {
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
+	@Override
+	public String toString() {
+		return name + "(" + sdf.format(birthDate) + ") - " + email;
+	}
+	
 	
 	
 	
